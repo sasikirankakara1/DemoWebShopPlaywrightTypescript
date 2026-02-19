0ce150099fe1e33dd7f0e46730016b9ebf26724d
sasikirankakara1 <sasikirankakaraprince@gmail.com>
2026-02-20 02:07:51 +0530

    hi

--- pages/DemoWebShopHomePage.ts
+++ pages/DemoWebShopHomePage.ts
@@ -4,9 +4,11 @@ export class DemoWebShopHomePage{
 
     readonly register:Locator
     readonly page:Page
+    
     constructor(page:Page){
         this.page=page
         this.register = page.locator("a[href*='/register']")
+      
     }
     async clickRegister(){
         return await this.register.click()
@@ -14,5 +16,6 @@ export class DemoWebShopHomePage{
     async gotoPage(url:string){
         return  this.page.goto(url)
     }
+  
 }
 
